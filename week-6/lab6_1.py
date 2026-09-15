e01b08062457be66b2f2e12df8995f6516f0eb96
jairamcharanch <jairamcharanch@gmail.com>
2026-09-15 11:10:20 +0530

    Add files via upload

---
+++ week-6/lab6_1.py
@@ -0,0 +1,60 @@
+class Stack:
+    def __init__(self,size):
+        self.size = size
+        self.stack = [None] * size
+        self.top = -1
+
+    def push(self,item):
+        if self.top == self.size - 1:
+            print("Stack Overflow")
+        else:
+            self.top += 1
+            self.stack[self.top] = item
+            print(item,"pushed into the stack")
+    def pop(self):
+        if self.top == -1:
+            print("Stack Underflow")
+        else:
+            item = self.stack[self.top]
+            self.stack[self.top] = None
+            self.top -= 1
+            print(item,"popped from the stack")
+    def peek(self):
+        if self.top == -1:
+            print("Stack is empty")
+        else:
+            print("Top element:",self.stack[self.top])
+    def display(self):
+        if self.top == -1:
+            print("Stack is empty")
+        else:
+            print("The elements of the stack:")
+            for i in range(self.top,-1,-1):
+                print(self.stack[i])
+
+size = int(input("Enter the size of the stack:"))
+s = Stack(size)
+while True:
+    print("--STACK MENU--")
+    print("1.Push")
+    print("2.Pop")
+    print("3.Peek")
+    print("4.Display")
+    print("5.Exit")
+
+    choice = int(input("Enter your choice:"))
+
+    if choice == 1:
+        item = int(input("Enter the element to push:"))
+        s.push(item)
+    elif choice == 2:
+        s.pop()
+    elif choice == 3:
+        s.peek()
+    elif choice == 4:
+        s.display()
+    elif choice == 5:
+        print("Exiting program")
+        break
+    else:
+        print("Invalid choice")
---
+++ week-6/lab6_2.py
@@ -0,0 +1,67 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Stack:
+    def __init__(self):
+        self.top = None
+
+    def push(self, item):
+        new = Node(item)
+        new.next = self.top
+        self.top = new
+        print(item, "pushed into the stack")
+
+    def pop(self):
+        if self.top is None:
+            print("Stack Underflow")
+        else:
+            temp = self.top
+            self.top = self.top.next
+            print(temp.data, "popped from the stack")
+
+    def peek(self):
+        if self.top is None:
+            print("Stack is empty")
+        else:
+            print("Top element:", self.top.data)
+
+    def display(self):
+        if self.top is None:
+            print("Stack is empty")
+        else:
+            print("The elements of the stack:")
+            temp = self.top
+            while temp is not None:
+                print(temp.data)
+                temp = temp.next
+
+
+s = Stack()
+
+while True:
+    print("\n--STACK MENU--")
+    print("1. Push")
+    print("2. Pop")
+    print("3. Peek")
+    print("4. Display")
+    print("5. Exit")
+
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+        item = int(input("Enter the element to push: "))
+        s.push(item)
+    elif choice == 2:
+        s.pop()
+    elif choice == 3:
+        s.peek()
+    elif choice == 4:
+        s.display()
+    elif choice == 5:
+        print("Exiting program")
+        break
+    else:
+        print("Invalid choice")
